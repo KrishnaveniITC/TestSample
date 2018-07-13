501c95b1adffcab0654cd6dc459d8b2fa5cd57f6
krishnaveni <krishnaveni.M@itcinfotech.com>
2018-07-13 16:52:26 +0530

    practise commands commiting code into branch

--- firstcommit/firstcommit/ViewController.swift
+++ firstcommit/firstcommit/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("commands practise")
         
         // Do any additional setup after loading the view, typically from a nib.
     }
